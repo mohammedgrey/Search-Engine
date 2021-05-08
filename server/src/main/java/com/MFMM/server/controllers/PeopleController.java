a2bf3eb4e8a6b28a2cc4516fe0c75ffc724f74da
MoaazZaki <moaazzaki1999@gmail.com>
2021-05-09 00:49:44 +0200

    Added spring framework basics

---
+++ server/src/main/java/com/MFMM/server/controllers/PeopleController.java
@@ -0,0 +1,33 @@
+package com.MFMM.server.controllers;
+
+import java.util.List;
+import com.MFMM.server.models.People;
+import com.MFMM.server.models.PeopleRepository;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+
+@RestController
+public class PeopleController {
+    
+    private final PeopleRepository peopleRepository;
+
+    @Autowired
+    public PeopleController(PeopleRepository peopleRepository)
+    {
+        this.peopleRepository = peopleRepository;
+    }
+
+    @GetMapping("/people")
+    public List<People> getPeople(){
+        return peopleRepository.findAll();
+    }
+
+    @PostMapping("/people/add")
+    public void addPeople(){
+        peopleRepository.save(new People("Mohammed","Saad"));
+    }
+}
