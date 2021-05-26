95b3d9c4b90178f1e8c19df7108d1c962c58f0ff
MoaazZaki <moaazzaki1999@gmail.com>
2021-05-26 16:30:59 +0200

    Added helpers function for Url handling and snippet extraction

---
+++ server/src/main/java/com/MFMM/server/helpers/Snippet.java
@@ -0,0 +1,121 @@
+package com.MFMM.server.helpers;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import java.util.HashSet;
+import java.util.Hashtable;
+
+public class Snippet {
+	final Integer STRING_MIN_LENGTH = 100;
+    private String getStringOfQueue(Queue<String> queue,Integer end)
+    {
+        Integer limitCounter = Integer.valueOf(0);
+        StringBuilder result = new StringBuilder();
+        for (String word : queue) {
+            if(limitCounter == end) break;
+            result.append(word + " ");
+            limitCounter++;
+        }
+
+        return result.toString().substring(0, result.length() - 1);
+    }
+
+    private String extendSnippet(String resultText,String[] wholeText,Integer startIndex, Integer numberOfElements)
+    {
+        if(startIndex != 0)
+        {
+            for (int i = startIndex - 1; i >= 0; i--) {
+                resultText = wholeText[i] + " " + resultText;
+                if(resultText.length() >= STRING_MIN_LENGTH) return resultText;
+            }
+        }
+        else if (startIndex + numberOfElements < wholeText.length)
+        {
+            for (int i = startIndex + numberOfElements; i < wholeText.length; i++) {
+                resultText = resultText + " " + wholeText[i];
+                if(resultText.length() >= STRING_MIN_LENGTH) return resultText;
+            }
+        }
+        
+        return resultText;
+    }
+
+    public String getSnippet(String[] wholeText, List<String> keywords) {
+        
+        // Initialize needed data structures 
+        Queue<String> matchQueue = new LinkedList<String>();
+        Hashtable<String,Integer> processedKeyWords = new Hashtable<String,Integer>();
+        HashSet<String> allKeyWords = new HashSet<String>(keywords);
+
+        // Initialize result variables
+        String result = new String("");
+        Integer minSize = Integer.MAX_VALUE;
+        Integer maxKeyWord = Integer.MIN_VALUE;
+        Integer startIndex = 0;
+
+        // Initialize Traversing variables
+        boolean matchFound = false;
+        boolean isKeyWord = false;
+        Integer nonkeyWordSize = 0;
+        Integer keyWordSize = 0;
+
+        for (String word : wholeText) {
+
+            // (1) Check if word is keyword
+            isKeyWord = allKeyWords.contains(word);
+            if (isKeyWord)
+            {
+                String currentNormalizingWord = word;
+                ///(2.1) Check if the keyword is the left of queue
+                Outloop: while(currentNormalizingWord.equals(matchQueue.peek()))
+                {
+
+                    // Store cuurent result as the best 
+                    if(matchQueue.size() - nonkeyWordSize < minSize && maxKeyWord <= keyWordSize)
+                    {
+                        minSize = matchQueue.size() - nonkeyWordSize;
+                        maxKeyWord = keyWordSize;
+                        result = getStringOfQueue(matchQueue,minSize); 
+                    }
+
+
+                    if(! currentNormalizingWord.equals(word) && processedKeyWords.get(currentNormalizingWord) == 1) break;
+                    ////(2.1.1) Remove the word from queue
+                    boolean isNotKeyWord = true;
+                    keyWordSize--;
+                    while(isNotKeyWord)
+                    {
+                        startIndex++;
+                        matchQueue.remove();
+                        if(matchQueue.isEmpty())
+                        {
+                            matchFound = false; //Don't insert to queue untill you find a keyword
+                            break Outloop;
+                        }
+                        isNotKeyWord = ! allKeyWords.contains(matchQueue.peek()); // Break if the word is a keyword
+                    }
+                    processedKeyWords.put(currentNormalizingWord, processedKeyWords.get(currentNormalizingWord) - 1); //Decrement current word counter
+                    currentNormalizingWord = matchQueue.peek(); //New keyword to look for
+                }
+
+                ///(2.2) Add new keyword to hashtable
+                processedKeyWords.put(word, processedKeyWords.getOrDefault(word,0) + 1);
+                keyWordSize++;
+                matchFound = true;
+                nonkeyWordSize = 0;
+            }
+            else
+            {
+                nonkeyWordSize++;
+            }
+
+            // (2) Inset to queue
+            if(matchFound || isKeyWord) matchQueue.add(word);
+        }
+
+        // Check if the string is long enough
+        if (result.length() < STRING_MIN_LENGTH) return extendSnippet(result,wholeText,startIndex,minSize);
+
+        return result;
+    }
+}
