34283a8dd424a537cb34c8eb672ad14f8b263037
mohammedgrey <mohammeddsaadd@yahoo.com>
2021-06-05 02:40:00 +0200

    Minor adjustment in the loading gif

--- look-me-up/src/components/LoadingGIF.js
+++ look-me-up/src/components/LoadingGIF.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function LoadingGIF() {
   return (
     <div className="center-me">
-      <img className="loading-gif rotate linear infinite" src={"https://s6.gifyu.com/images/loading24e03d245dbdcc50.gif"} alt="loading..." />;
+      <img className="loading-gif rotate linear infinite" src={"https://cdn.discordapp.com/attachments/626125078521708554/850515439577726996/loading.gif"} alt="loading..." />;
     </div>
   );
 }
